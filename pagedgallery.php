cf7afa629da5f889486bcda2324742f2001bbefb
Thibault Milan <clawfire@gmail.com>
2010-11-03 11:04:14 +0100

    first commit

---
+++ pagedgallery.php
@@ -0,0 +1,13 @@
+<?php /*
+	Paged Gallery, v0.1
+	thibault milan, 2010. 
+	hello@thibaultmilan.com
+	http://thibaultmilan.com
+	if you like this plugin please visit our site and donate or hire us do some work for you!
+	
+	This software is licensed under the <a href="http://creativecommons.org/licenses/GPL/2.0/">CC-GNU GPL</a> version 2.0 or later,.
+	
+	thibault milan or empowebo are in no way responsible for any damage, software vulnerability, data issues or technical problems arising from this software
+	Inspired by yCyclista from <a href="http://social-ink.net">yonatan reinberg</a>
+
+*/ ?>
